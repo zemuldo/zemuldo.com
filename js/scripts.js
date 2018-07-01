448e6091c8ba5c941ab62d550f2f5d7e8476c56d
Danstan Onyango <danstan.otieno@gmail.com>
2018-07-02 00:17:20 +0530

    adds change isons and favicons

--- js/scripts.js
+++ js/scripts.js
@@ -47,7 +47,7 @@
 		
        /* Smooth Scroll */
 
-        $('a.smoth-scroll').on("click", function (e) {
+        $('a.smoth-scroll').on('click', function (e) {
             var anchor = $(this);
             $('html, body').stop().animate({
                 scrollTop: $(anchor.attr('href')).offset().top - 50
@@ -109,8 +109,8 @@
         /* Typed.js */
 
         $(window).load(function(){
-        $(".typed").typed({
-            strings: ["I am A Software Developer. ^1000 I Design WebApplications and POS Systems. I am also a Systems Administrator. I design, Implement and Maintain Network Infrastructure. I am also a professional Linux User. Graduate in Telecommunications Engineering. I Live in Nairobi. <br> My Expertise are in: <br> 1. ^1000  Java EE <br> 2. ^1000  NodeJs-AngulaJS <br> 3. ^1000  Database Design ^1000 <br> and  ofcoz  Much More... ^1000 <br>You wanna find  out More? or Contact me? Check My Profile Down There &#x1f447 <br> Dont forget to Leave a message "],
+        $('.typed').typed({
+            strings: ['I am A Software Developer. ^1000 I Design WebApplications and POS Systems. I am also a Systems Administrator. I design, Implement and Maintain Network Infrastructure. I am also a professional Linux User. Graduate in Telecommunications Engineering. I Live in Nairobi. <br> My Expertise are in: <br> 1. ^1000  Java EE <br> 2. ^1000  NodeJs-AngulaJS <br> 3. ^1000  Database Design ^1000 <br> and  ofcoz  Much More... ^1000 <br>You wanna find  out More? or Contact me? Check My Profile Down There &#x1f447 <br> Dont forget to Leave a message '],
 
           });
          });
@@ -182,12 +182,12 @@
 		 
         /* Testimonial Carousel/Slider */
 
-        $(".testimonial-carousel-list").owlCarousel({
+        $('.testimonial-carousel-list').owlCarousel({
             items: 1,
             autoPlay: true,
             stopOnHover: true,
             navigation: true,
-            navigationText: ["<i class='fa fa-long-arrow-left fa-2x owl-navi'></i>", "<i class='fa fa-long-arrow-right fa-2x owl-navi'></i>"],
+            navigationText: ['<i class=\'fa fa-long-arrow-left fa-2x owl-navi\'></i>', '<i class=\'fa fa-long-arrow-right fa-2x owl-navi\'></i>'],
             itemsDesktop: [1199, 1],
             itemsDesktopSmall: [980, 1],
             itemsTablet: [768, 1],
@@ -195,7 +195,7 @@
             itemsMobile: [479, 1],
             autoHeight: true,
             pagination: false,
-            transitionStyle : "backSlide"
+            transitionStyle : 'backSlide'
         });
 		
 		
@@ -239,8 +239,8 @@
             maptype: 'ROADMAP',
             markers:[
                     {
-                        address: "Kenyatta Avenue, Nairobi",  /* You can change your address from here */
-                        html: "<b>24136-00100</b>: <br> GPO, Nairobi, Kenya.",   /* You can change display address text from here */
+                        address: 'Kenyatta Avenue, Nairobi',  /* You can change your address from here */
+                        html: '<b>24136-00100</b>: <br> GPO, Nairobi, Kenya.',   /* You can change display address text from here */
                         popup: true
                     }
                 ]
@@ -253,17 +253,17 @@
 
 //Matrix Background canvas
 // geting canvas by id c
-var c = document.getElementById("c");
-var ctx = c.getContext("2d");
+var c = document.getElementById('c');
+var ctx = c.getContext('2d');
 
 //making the canvas full screen
 c.height = window.innerHeight;
 c.width = window.innerWidth;
 
 //chinese characters - taken from the unicode charset
-var matrix = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%";
+var matrix = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%';
 //converting the string into an array of single characters
-matrix = matrix.split("");
+matrix = matrix.split('');
 
 var font_size = 10;
 var columns = c.width/font_size; //number of columns for the rain
@@ -279,11 +279,11 @@ function draw()
 {
     //Black BG for the canvas
     //translucent BG to show trail
-    ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.04)';
     ctx.fillRect(0, 0, c.width, c.height);
 
-    ctx.fillStyle = "#0F0"; //green text
-    ctx.font = font_size + "px arial";
+    ctx.fillStyle = '#0F0'; //green text
+    ctx.font = font_size + 'px arial';
     //looping over drops
     for(var i = 0; i < drops.length; i++)
     {
--- js/typed.js
+++ js/typed.js
@@ -1,8 +1,8 @@
 
 /* Typed.js */ ! function(t) {
-    "use strict";
+    'use strict';
     var s = function(s, e) {
-        this.el = t(s), this.options = t.extend({}, t.fn.typed.defaults, e), this.isInput = this.el.is("input"), this.attr = this.options.attr, this.showCursor = this.isInput ? !1 : this.options.showCursor, this.elContent = this.attr ? this.el.attr(this.attr) : this.el.text(), this.contentType = this.options.contentType, this.typeSpeed = this.options.typeSpeed, this.startDelay = this.options.startDelay, this.backSpeed = this.options.backSpeed, this.backDelay = this.options.backDelay, this.stringsElement = this.options.stringsElement, this.strings = this.options.strings, this.strPos = 0, this.arrayPos = 0, this.stopNum = 0, this.loop = this.options.loop, this.loopCount = this.options.loopCount, this.curLoop = 0, this.stop = !1, this.cursorChar = this.options.cursorChar, this.shuffle = this.options.shuffle, this.sequence = [], this.build()
+        this.el = t(s), this.options = t.extend({}, t.fn.typed.defaults, e), this.isInput = this.el.is('input'), this.attr = this.options.attr, this.showCursor = this.isInput ? !1 : this.options.showCursor, this.elContent = this.attr ? this.el.attr(this.attr) : this.el.text(), this.contentType = this.options.contentType, this.typeSpeed = this.options.typeSpeed, this.startDelay = this.options.startDelay, this.backSpeed = this.options.backSpeed, this.backDelay = this.options.backDelay, this.stringsElement = this.options.stringsElement, this.strings = this.options.strings, this.strPos = 0, this.arrayPos = 0, this.stopNum = 0, this.loop = this.options.loop, this.loopCount = this.options.loopCount, this.curLoop = 0, this.stop = !1, this.cursorChar = this.options.cursorChar, this.shuffle = this.options.shuffle, this.sequence = [], this.build()
     };
     s.prototype = {
         constructor: s,
@@ -15,7 +15,7 @@
         },
         build: function() {
             var s = this;
-            if (this.showCursor === !0 && (this.cursor = t('<span class="typed-cursor">' + this.cursorChar + "</span>"), this.el.after(this.cursor)), this.stringsElement) {
+            if (this.showCursor === !0 && (this.cursor = t('<span class="typed-cursor">' + this.cursorChar + '</span>'), this.el.after(this.cursor)), this.stringsElement) {
                 this.strings = [], this.stringsElement.hide(), console.log(this.stringsElement.children());
                 var e = this.stringsElement.children();
                 t.each(e, function(e, i) {
@@ -31,16 +31,16 @@
                 i.timeout = setTimeout(function() {
                     var e = 0,
                         r = t.substr(s);
-                    if ("^" === r.charAt(0)) {
+                    if ('^' === r.charAt(0)) {
                         var o = 1;
                         /^\^\d+/.test(r) && (r = /\d+/.exec(r)[0], o += r.length, e = parseInt(r)), t = t.substring(0, s) + t.substring(s + o)
                     }
-                    if ("html" === i.contentType) {
+                    if ('html' === i.contentType) {
                         var n = t.substr(s).charAt(0);
-                        if ("<" === n || "&" === n) {
-                            var a = "",
-                                h = "";
-                            for (h = "<" === n ? ">" : ";"; t.substr(s + 1).charAt(0) !== h && (a += t.substr(s).charAt(0), s++, !(s + 1 > t.length)););
+                        if ('<' === n || '&' === n) {
+                            var a = '',
+                                h = '';
+                            for (h = '<' === n ? '>' : ';'; t.substr(s + 1).charAt(0) !== h && (a += t.substr(s).charAt(0), s++, !(s + 1 > t.length)););
                             s++, a += h
                         }
                     }
@@ -53,7 +53,7 @@
                         } else {
                             0 === s && i.options.preStringTyped(i.arrayPos);
                             var e = t.substr(0, s + 1);
-                            i.attr ? i.el.attr(i.attr, e) : i.isInput ? i.el.val(e) : "html" === i.contentType ? i.el.html(e) : i.el.text(e), s++, i.typewrite(t, s)
+                            i.attr ? i.el.attr(i.attr, e) : i.isInput ? i.el.val(e) : 'html' === i.contentType ? i.el.html(e) : i.el.text(e), s++, i.typewrite(t, s)
                         }
                     }, e)
                 }, e)
@@ -64,13 +64,13 @@
                 var e = Math.round(70 * Math.random()) + this.backSpeed,
                     i = this;
                 i.timeout = setTimeout(function() {
-                    if ("html" === i.contentType && ">" === t.substr(s).charAt(0)) {
-                        for (var e = "";
-                             "<" !== t.substr(s - 1).charAt(0) && (e -= t.substr(s).charAt(0), s--, !(0 > s)););
-                        s--, e += "<"
+                    if ('html' === i.contentType && '>' === t.substr(s).charAt(0)) {
+                        for (var e = '';
+                             '<' !== t.substr(s - 1).charAt(0) && (e -= t.substr(s).charAt(0), s--, !(0 > s)););
+                        s--, e += '<'
                     }
                     var r = t.substr(0, s);
-                    i.attr ? i.el.attr(i.attr, r) : i.isInput ? i.el.val(r) : "html" === i.contentType ? i.el.html(r) : i.el.text(r), s > i.stopNum ? (s--, i.backspace(t, s)) : s <= i.stopNum && (i.arrayPos++, i.arrayPos === i.strings.length ? (i.arrayPos = 0, i.shuffle && (i.sequence = i.shuffleArray(i.sequence)), i.init()) : i.typewrite(i.strings[i.sequence[i.arrayPos]], s))
+                    i.attr ? i.el.attr(i.attr, r) : i.isInput ? i.el.val(r) : 'html' === i.contentType ? i.el.html(r) : i.el.text(r), s > i.stopNum ? (s--, i.backspace(t, s)) : s <= i.stopNum && (i.arrayPos++, i.arrayPos === i.strings.length ? (i.arrayPos = 0, i.shuffle && (i.sequence = i.shuffleArray(i.sequence)), i.init()) : i.typewrite(i.strings[i.sequence[i.arrayPos]], s))
                 }, e)
             }
         },
@@ -83,18 +83,18 @@
         reset: function() {
             var t = this;
             clearInterval(t.timeout);
-            this.el.attr("id");
-            this.el.empty(), "undefined" != typeof this.cursor && this.cursor.remove(), this.strPos = 0, this.arrayPos = 0, this.curLoop = 0, this.options.resetCallback()
+            this.el.attr('id');
+            this.el.empty(), 'undefined' != typeof this.cursor && this.cursor.remove(), this.strPos = 0, this.arrayPos = 0, this.curLoop = 0, this.options.resetCallback()
         }
     }, ((t || {}).fn || {}).typed = function(e) {
         return this.each(function() {
             var i = t(this),
-                r = i.data("typed"),
-                o = "object" == typeof e && e;
-            r && r.reset(), i.data("typed", r = new s(this, o)), "string" == typeof e && r[e]()
+                r = i.data('typed'),
+                o = 'object' == typeof e && e;
+            r && r.reset(), i.data('typed', r = new s(this, o)), 'string' == typeof e && r[e]()
         })
     }, (((t || {}).fn || {}).typed || {}).defaults = {
-        strings: ["These are the default values...", "You know what you should do?", "Use your own!", "Have a great day!"],
+        strings: ['These are the default values...', 'You know what you should do?', 'Use your own!', 'Have a great day!'],
         stringsElement: null,
         typeSpeed: 60,
         startDelay: 0,
@@ -104,9 +104,9 @@
         loop: !0,
         loopCount: !0,
         showCursor: !0,
-        cursorChar: "|",
+        cursorChar: '|',
         attr: null,
-        contentType: "html",
+        contentType: 'html',
         callback: function() {},
         preStringTyped: function() {},
         onStringTyped: function() {},
