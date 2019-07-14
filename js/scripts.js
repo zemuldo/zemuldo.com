b909f51361e5349c9e212a299eb950e13fe52529
Danstan Onyango <danstan.otieno@gmail.com>
2019-07-14 17:30:21 +0300

    new views

--- js/scripts.js
+++ js/scripts.js
@@ -1,259 +1,30 @@
-/*
-   
-    Template Name : Rolling - Freelancer Portfolio Template
-    Author : UiPasta Team
-    Website : http://www.uipasta.com/
-    Support : http://www.uipasta.com/support/
-	
-	
-*/
-
-
-
-/*
-   
-   Table Of Content
-   
-   1. Preloader
-   2. Smooth Scroll
-   3. Scroll Naviagation Background Change with Sticky Navigation
-   4. Mobile Navigation Hide or Collapse on Click
-   5. Scroll To Top
-   6. Typed.js
-   7. Parallax Background
-   8. Portfolio Filtering
-   9. Magnific Popup
-  10. Testimonial Carousel/Slider
-  11. Statistics Counter
-  12. Google Map
- 
-
-*/
-
-
-(function ($) {
-    'use strict';
-
-    jQuery(document).ready(function () {
-
-
-        /* Preloader */
-
-        $(window).on('load', function () {
-            $('body').addClass('loaded');
-        });
-
-
-
-        /* Smooth Scroll */
-
-        $('a.smoth-scroll').on('click', function (e) {
-            var anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $(anchor.attr('href')).offset().top - 50
-            }, 1000);
-            e.preventDefault();
-        });
-
-
-
-
-        /* Scroll Naviagation Background Change with Sticky Navigation */
-
-        $(window).on('scroll', function () {
-            if ($(window).scrollTop() > 100) {
-                $('.header-top-area').addClass('navigation-background');
-            } else {
-                $('.header-top-area').removeClass('navigation-background');
-            }
-        });
-
-
-
-
-        /* Mobile Navigation Hide or Collapse on Click */
-
-        $(document).on('click', '.navbar-collapse.in', function (e) {
-            if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
-                $(this).collapse('hide');
-            }
-        });
-        $('body').scrollspy({
-            target: '.navbar-collapse',
-            offset: 195
-
-        });
-
-
-
-
-        /* Scroll To Top */
-
-        $(window).scroll(function () {
-            if ($(this).scrollTop() >= 500) {
-                $('.scroll-to-top').fadeIn();
-            } else {
-                $('.scroll-to-top').fadeOut();
-            }
-        });
-
-
-        $('.scroll-to-top').click(function () {
-            $('html, body').animate({ scrollTop: 0 }, 800);
-            return false;
-        });
-
-
-
-
-        /* Typed.js */
-
-        function whiten (string) {
-            return '<span style="color: white;">' + string +  '</span>'
-        }
-
-        $(window).load(function () {
-
-            var waitAndBreak = '^700 <br/>';
-            var start = '<span class="margin-left--10 color-blue font-size-14 !important">➜:~</span>';
-            var myStack = start + whiten('Skill Set and Stack?') + waitAndBreak + ['Nodejs', 'ReactJS', 'Elixir', 'Docker', 'Python for Data', 'Kubernets', 'Google Cloud Platform' ,'and Amazon WebServices Cloud.'].join(',^500 ');
-            var experience = 'I have 3 years of professional experience developing software using NodeJS and ReactJS' +  '^500 and a year working with Elixir.' + '^500 Docker and Kubernetes have been a part of my work with Node and Elixir.'
-            var yearsOfExperience = waitAndBreak + start + whiten('Years of Experience?') + waitAndBreak + experience;
-            var funGames = ['During my free time I always either doing all or one of ' + 'playing Chess', 'or am cooking', 'or listening to music and dancing.'].join('^500, ')
-            var currentStatus = waitAndBreak + start + whiten('My Fun time?') + waitAndBreak + funGames;
-            var coolStuff = ['I do mentorship and also write content on medium.', 'I also am good with TCP/IP and Computer Networks.'].join('^500, ')
-            var someOtherCoolStuffICanDo = waitAndBreak + start + whiten('Any cool stuff I can do?') + waitAndBreak + coolStuff;
-            $('.typed').typed({
-                strings: [myStack + yearsOfExperience + currentStatus + someOtherCoolStuffICanDo],
-
-            });
-        });
-
-        /* Parallax Background */
-
-        $(window).stellar({
-            responsive: true,
-            horizontalScrolling: false,
-            hideDistantElements: false,
-            horizontalOffset: 0,
-            verticalOffset: 0,
-        });
-
-
-
-
-        /* Portfolio Filtering */
-
-        $('.portfolio-inner').mixItUp();
-
-
-
-        /* Magnific Popup */
-
-        $('.portfolio-popup').magnificPopup({
-            type: 'image',
-
-            gallery: { enabled: true },
-            zoom: {
-                enabled: true,
-                duration: 500
-
-            },
-
-            image: {
-                markup: '<div class="mfp-figure portfolio-pop-up">' +
-                    '<div class="mfp-close"></div>' +
-                    '<div class="mfp-img"></div>' +
-                    '<div class="mfp-bottom-bar portfolio_title">' +
-                    '<div class="mfp-title"></div>' +
-                    '<div class="mfp-counter"></div>' +
-                    '</div>' +
-                    '</div>',
-
-                titleSrc: function (item) {
-                    return item.el.attr('title');
-                }
-            }
-
-
-        });
-
-
-
-
-        /* Testimonial Carousel/Slider */
-
-        $('.testimonial-carousel-list').owlCarousel({
-            items: 1,
-            autoPlay: true,
-            stopOnHover: true,
-            navigation: true,
-            navigationText: ['<i class=\'fa fa-long-arrow-left fa-2x owl-navi\'></i>', '<i class=\'fa fa-long-arrow-right fa-2x owl-navi\'></i>'],
-            itemsDesktop: [1199, 1],
-            itemsDesktopSmall: [980, 1],
-            itemsTablet: [768, 1],
-            itemsTabletSmall: false,
-            itemsMobile: [479, 1],
-            autoHeight: true,
-            pagination: false,
-            transitionStyle: 'backSlide'
-        });
-
-
-
-
-        /* Statistics Counter */
-
-        $('.statistics').appear(function () {
-            var counter = $(this).find('.statistics-count');
-            var toCount = counter.data('count');
-
-            $(counter).countTo({
-                from: 0,
-                to: toCount,
-                speed: 5000,
-                refreshInterval: 50
-            })
-        });
-
-
-
-        /* Google Map */
-        var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
-        var icons = {
-            parking: {
-                name: 'Parking',
-                icon: iconBase + 'parking_lot_maps.png'
-            },
-            library: {
-                name: 'Library',
-                icon: iconBase + 'library_maps.png'
-            },
-            info: {
-                name: 'Info',
-                icon: iconBase + 'info-i_maps.png'
-            }
-        };
-        $('#my-address').gMap({
-            zoom: 5,
-            scrollwheel: true,
-            maptype: 'ROADMAP',
-            markers: [
-                {
-                    address: 'Kenyatta Avenue, Nairobi',  /* You can change your address from here */
-                    html: '<b>24136-00100</b>: <br> GPO, Nairobi, Kenya.',   /* You can change display address text from here */
-                    popup: true
-                }
-            ]
-        });
-
+/* Typed.js */
+
+function whiten (string) {
+    return '<span style="color: white;">' + string +  '</span>'
+}
+
+function typeOnTerminal () {
+
+    var waitAndBreak = '^700 <br/>';
+    var start = '<span class="margin-left--10 color-blue font-size-14 !important">➜:~</span>';
+    var myStack = start + whiten('Skill Set and Stack?') + waitAndBreak + ['Nodejs', 'ReactJS', 'Elixir', 'Docker', 'Python for Data', 'K8s', 'GCP' ,'and AWs.'].join(',^500 ');
+    var experience = '3 years';
+    var yearsOfExperience = waitAndBreak + start + whiten('Years of Experience?') + waitAndBreak + experience;
+    var funGames = ['Chess,' + ' Music and ' +  'Dancing'].join('^500, ');
+    var currentStatus = waitAndBreak + start + whiten('My Fun time?') + waitAndBreak + funGames;
+    var coolStuff = ['I do mentorship and also write content on medium.'];
+    var someOtherCoolStuffICanDo = waitAndBreak + start + whiten('Any cool stuff I can do?') + waitAndBreak + coolStuff;
+    $('.typed').typed({
+        strings: [myStack + yearsOfExperience + currentStatus],
 
     });
+}
 
-})(jQuery);
+/*
+    Matrix Background canvas
+*/
 
-//Matrix Background canvas
-// geting canvas by id c
 var c = document.getElementById('c');
 var ctx = c.getContext('2d');
 
@@ -302,38 +73,4 @@ function draw() {
 }
 
 setInterval(draw, 35);
-$(document).ready(function () {
-    // Listen to click event on the submit button
-    $('#send-message').click(function (e) {
-
-        e.preventDefault();
-
-        var name = $("#name").val();
-        var email = $("#name").val();
-        var address = $("#address").val();
-        var subject = $("#subject").val();
-        var message = $("#message").val();
-        var website = $("#website").val()
-
-        if (!name || !email || !address || !message) return alert("Please fill form")
-
-        var text = JSON.stringify({ name, email, address, subject, message, website })
-        var url = "https://hooks.slack.com/services/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"
-        $.ajax({
-            data: 'payload=' + JSON.stringify({
-                "text": text
-            }),
-            dataType: 'text',
-            processData: false,
-            type: 'POST',
-            url: url
-        }).done(function()  {
-            $( "#leavemessage-container").replaceWith( "<div class=\"col-md-6\"><h4 class=\"center-align\">Thanks for your message</h4></div>" );
-        }).fail(function(e)  {
-            alert("Sorry. Server unavailable. ");
-        });
-    });
-
-
-});
 
