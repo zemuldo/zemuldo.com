d4997546959099c681654294a3bfacddd12afce2
Danstan Onyango <danstan.otieno@gmail.com>
2019-07-20 18:00:13 +0300

    adds format date

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,9 @@ function getYearsBetween(sdt) {
             years += 1;
             return '';
         }
+        if (months == 1) {
+            return ' and ' + months + ' Month';
+        }
         return ' and ' + months + ' Months';
     };
     return years + ' Years' + months(date_difference.getMonth() + 1);
