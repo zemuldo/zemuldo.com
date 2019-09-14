c1917855dc27c9247438a43764abd3fe07372e7e
Danstan Onyango <danstan.otieno@gmail.com>
2019-09-14 16:58:37 +0300

    adds loader

--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,20 @@ import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../src/theme';
-import NProgress from 'nprogress'
-import Router from 'next/router'
+import NProgress from 'nprogress';
+import Router from 'next/router';
 
-Router.events.on('routeChangeStart', url => {
-  NProgress.start()
-})
-Router.events.on('routeChangeComplete', () => NProgress.done())
-Router.events.on('routeChangeError', () => NProgress.done())
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+};
+
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+};
+
+Router.onRouteChangeError = () => {
+  NProgress.done();
+};
 
 class MyApp extends App {
   componentDidMount() {
