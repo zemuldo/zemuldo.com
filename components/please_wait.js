592d2c73c248290532b0d7197aed0eb33f753ba6
Danstan Onyango <danstan.otieno@gmail.com>
2019-10-09 21:20:00 +0300

    rearrange menu

---
+++ components/please_wait.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+
+export default function PleaseWait() {
+    
+  return (
+    <React.Fragment>
+      <Container style={{ color: 'white' }} maxWidth="md">
+
+        <Grid container justify="center" alignItems="center">
+          <Grid container spacing={4}>
+            <div className='center-content'>
+              <h1>Please wait</h1>
+            </div>
+          </Grid>
+        </Grid>
+      </Container>
+    </React.Fragment>
+  );
+}
