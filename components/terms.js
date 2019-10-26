582886bdf742becaa1e83f156e87aa110dd80f7c
Danstan Onyango <danstan.otieno@gmail.com>
2019-10-26 18:53:08 +0300

    fix bug

--- components/terms.js
+++ components/terms.js
@@ -15,9 +15,7 @@ export default function AcceptTerms({accepted_terms}) {
   if (accepted_terms === '1' || accepted_terms_now === '1') return null;
   return (
     <React.Fragment>
-      <br/>
-      <br/>
-      <br/>
+      
       <div style={{
         textAlign: 'center',
         position: 'fixed',
