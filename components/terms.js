e7f97d5da02a4b2384287a1a114e974e2455264b
Danstan Onyango <danstan.otieno@gmail.com>
2019-10-29 09:43:20 +0300

    fix cookie modal

--- components/terms.js
+++ components/terms.js
@@ -24,7 +24,7 @@ export default function AcceptTerms({accepted_terms}) {
         color: 'white',
         display: 'flex',
         width: '100%',
-        backgroundColor: 'rgb(23, 23, 23',
+        backgroundColor: 'rgb(23, 23, 23)',
         margin: 'auto',
         alignContent: 'center',
         borderTop: 'solid 1px #08a6f3'
