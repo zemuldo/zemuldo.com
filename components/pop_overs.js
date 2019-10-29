d69f1d8f02cd7448a5a1d78b1bd2d51bdffebe2d
Danstan Onyango <danstan.otieno@gmail.com>
2019-10-29 19:01:56 +0300

    rm social sites popovers

--- components/pop_overs.js
+++ components/pop_overs.js
@@ -24,20 +24,19 @@ export default function MouseOverPopover({ children, hoverText, color }) {
       >
         {children}
       </span>
-      {
+      {/* {
         open &&
         <span style={{
           fontSize: '.8em',
           textAlign: 'center',
           color: color,
           position: 'absolute',
-          marginTop: '-20px',
           boxShadow: '0 2px 5px 0 rgba(0, 0, 0, 0.26)',
           transform: 'translate(0, 10px)',
           marginLeft: '-60px'
         }}>{hoverText}
         </span>
-      }
+      } */}
     </React.Fragment>
 
   );
