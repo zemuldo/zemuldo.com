c622b45cd8ada46b595df035078fc77d6b2645ad
Danstan Onyango <danstan.otieno@gmail.com>
2019-10-29 15:40:01 +0300

    adds release details

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,11 @@ import errorHandler from '../components/errorHandler';
 function Index() {
   return (
     <React.Fragment>
+      <div className='version-details'>
+          <div>@Version: {process.env.VERSION}</div>
+          <div>@Build: {process.env.BUILD.slice(0, 9)}</div>
+          <div className='branch'>@Branch: {process.env.RUNNING_BRANCH}</div>
+        </div>
       <Home/>
       <Footer/>
     </React.Fragment>
