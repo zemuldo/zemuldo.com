d0fde154571618caf157f5c9b6d034ffd8656dd9
Danstan Onyango <danstan.otieno@gmail.com>
2019-11-03 05:33:40 +0300

    update stacks

--- components/tools/stacks.js
+++ components/tools/stacks.js
@@ -1,14 +1,27 @@
 export  const options = [
   { value: 'javascript', label: 'JavaCript', color: '#fcba03' },
-  { value: 'reactjs', label: 'ReactJS', color: '#08a6f3' },
-  { value: 'postgresql', label: 'PostgreSQL', color: 'white' },
-  { value: 'elixir', label: 'Elixir', color: '#a767f5' },
   { value: 'nodejs', label: 'NodeJS', color: '#40a832' },
+  { value: 'reactjs', label: 'ReactJS', color: '#08a6f3' },
+  { value: 'nextjs', label: 'NextJS', color: 'white' },
+
+  { value: 'elixir', label: 'Elixir', color: '#a767f5' },
+  { value: 'gen-stage', label: 'GenStage', color: '#a767f5' },
+  { value: 'gen-server', label: 'GenServer', color: '#a767f5' },
   { value: 'ecto', label: 'Ecto', color: '#40a832' },
   { value: 'phoenix', label: 'Phoenix', color: 'orange' },
-  { value: 'python', label: 'Python', color: '#fff305' },
+
+  { value: 'postgresql', label: 'PostgreSQL', color: 'white' },
   { value: 'sql', label: 'SQL', color: '#05daff' },
   { value: 'data', label: 'Data', color: '#05daff' },
+  
+  { value: 'python', label: 'Python', color: '#fff305' },
+ 
   { value: 'kubernetes', label: 'Kubernetes', color: 'blue' },
-  { value: 'docker', label: 'Docker', color: '#08a6f3' }
+  { value: 'docker', label: 'Docker', color: '#08a6f3' },
+  
+  { value: 'git', label: 'Git', color: 'rgb(220, 71, 54)' },
+  { value: 'github-action', label: 'GitHub Actions', color: 'rgb(220, 71, 54)' },
+
+  { value: 'shell', label: 'Shell', color: 'green' },
+  { value: 'linux', label: 'Linux', color: 'white' }
 ];
