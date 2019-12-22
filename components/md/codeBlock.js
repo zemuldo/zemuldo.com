093f07f1ac8ccf80a549887d0547c4f6fe3de2a4
Danstan Onyango <danstan.otieno@gmail.com>
2019-12-22 08:34:48 +0300

    user dracula theme

--- components/md/codeBlock.js
+++ components/md/codeBlock.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import  theme from 'react-syntax-highlighter/dist/esm/styles/prism/xonokai';
+import  theme from 'react-syntax-highlighter/dist/esm/styles/prism/darcula';
 
 class CodeBlock extends PureComponent {
   static propTypes = {
