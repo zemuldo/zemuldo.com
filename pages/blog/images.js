3de726355bc65c6be95b299616e1271fdfadaa7b
Danstan Onyango <danstan.otieno@gmail.com>
2019-12-29 22:08:14 +0300

    adds image links

--- pages/blog/images.js
+++ pages/blog/images.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import fetch from 'isomorphic-unfetch';
@@ -31,11 +32,36 @@ const useStyles = makeStyles(theme => ({
   },
   cardContent: {
     flexGrow: 1,
-  }
+  },
+  floatingLabelFocusStyle: {
+    color: '#08a6f3',
+    '&:after': {
+      color: '#08a6f3',
+    },
+  },
+  materialInput: {
+    borderBottom: '1px solid green',
+    '&:after': {
+      borderBottom: '1px solid green',
+    },
+    fontSize: '28px',
+    color: '#08a6f3'
+  },
 }));
 
 function Album({images, authorization, user}) {
+  const [search, setSearch] = useState(null);
+  const [stateImages, setStateImages] = useState(images);
   const classes = useStyles();
+
+  const  fetchImages = async (e) => {
+    if (e.key === 'Enter') {
+      const res = await fetch(`${api_url}/image?skip=0&limit=50&search=${search}`);
+      const data = await res.json();
+      setStateImages(data);
+    }
+  };
+
   return (
     <React.Fragment>
       <Head>
@@ -49,8 +75,25 @@ function Album({images, authorization, user}) {
         </Grid>
       </Container>
       <Container style={{ color: 'white', backgroundColor: 'rgb(23, 23, 23)' }} maxWidth="md">
+        <TextField
+          id="coverPhotoUrl"
+          label="Search"
+          value={search}
+          fullWidth={true}
+          InputProps={{
+            className: classes.materialInput
+          }}
+          InputLabelProps={{
+            className: classes.floatingLabelFocusStyle,
+          }}
+          onKeyDown={fetchImages}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </Container>
+      <br/>
+      <Container style={{ color: 'white', backgroundColor: 'rgb(23, 23, 23)' }} maxWidth="md">
         <Grid container spacing={4}>
-          {images.map(image => (
+          {stateImages.map(image => (
             <Grid item key={image.name} xs={12} sm={6} md={4}>
               <Card className={classes.card}>
                 <CardMedia
