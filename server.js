45a8389c1bd0bc9d09c4c60edfe5e2fe0c3699e8
Danstan Onyango <danstan.otieno@gmail.com>
2020-04-02 12:53:22 +0300

    adds backward compatibility for /blog/post:meta

--- server.js
+++ server.js
@@ -67,6 +67,8 @@ app.prepare().then(() => {
 
   const server = express();
 
+  server.get('/blog/post/:meta', (req, _res) => app.render(req, _res, `/blog/${req.params.meta}`));
+
   server.use('/', router);
 
   server.use('*', (req, _res, next) => {
