72491401f515aad0afd15e110d6245248f42c6b8
Danstan <danstan.otieno@gmail.com>
2020-07-31 09:36:36 +0300

    adds more stacks

--- components/tools/stacks.js
+++ components/tools/stacks.js
@@ -5,10 +5,12 @@ export  const options = [
   { value: 'nextjs', label: 'NextJS', color: 'white' },
 
   { value: 'elixir', label: 'Elixir', color: '#a767f5' },
+  { value: 'erlang', label: 'Er-Lang', color: 'rgb(151, 0, 39)' },
   { value: 'gen-stage', label: 'GenStage', color: '#a767f5' },
   { value: 'gen-server', label: 'GenServer', color: '#a767f5' },
   { value: 'ecto', label: 'Ecto', color: '#40a832' },
   { value: 'phoenix', label: 'Phoenix', color: 'orange' },
+  { value: 'beam', label: 'OTP and BEAM', color: 'rgb(151, 0, 39)' },
 
   { value: 'postgresql', label: 'PostgreSQL', color: 'white' },
   { value: 'sql', label: 'SQL', color: '#05daff' },
@@ -18,6 +20,8 @@ export  const options = [
  
   { value: 'kubernetes', label: 'Kubernetes', color: 'blue' },
   { value: 'docker', label: 'Docker', color: '#08a6f3' },
+  { value: 'google-cloud', label: 'Google Cloud', color: '#08a6f3' },
+  { value: 'aws', label: 'AWS', color: '#08a6f3' },
   
   { value: 'git', label: 'Git', color: 'rgb(220, 71, 54)' },
   { value: 'github-action', label: 'GitHub Actions', color: 'rgb(220, 71, 54)' },
