a1686bedddbbb10ac0009190103e16474ed40827
Zemuldo <danstan.otieno@gmail.com>
2021-01-18 00:31:52 +0300

    fix linting

--- pages/404.js
+++ pages/404.js
@@ -1,53 +1,52 @@
 import React from 'react';
 import Head from 'next/head';
 import { Container } from '@material-ui/core';
-import { withRouter } from 'next/router';
 const Styles = {};
 
 const errorWrapper = (code, message) => {
-    return (<div 
-      style={{ 
-        color: '#000', 
-        fontFamily: '-apple-system, BlinkMacSystemFont, Roboto, "Segoe UI", "Fira Sans", Avenir, "Helvetica Neue", "Lucida Grande", sans-serif', 
-        height: '100vh', 
-        textAlign: 'center', 
-        display: 'flex', 
-        flexDirection: 'column', 
-        alignItems: 'center', 
-        justifyContent: 'center' }}
-    >
-      <div>
-        <h1 
-          style={{
-            display: 'inline-block', 
-            borderRight: '1px solid rgba(0, 0, 0,.3)', 
-            margin: 0, marginRight: '10px', 
-            padding: '10px 23px 10px 0', 
-            fontSize: '24px', 
-            fontWeight: 500, 
-            verticalAlign: 'top' }}
-        >
-          {code}
-          <span style={{color: '#fff', marginLeft: '20px'}}>|</span>
-        </h1>
-        <div style={{ display: 'inline-block', textAlign: 'left', lineHeight: '49px', height: '49px', verticalAlign: 'middle' }}>
-          <h2 style={{ fontSize: '14px', fontWeight: 'normal', lineHeight: 'inherit', margin: 0, padding: 0 }}>{message}{/* */}
-          </h2>
-        </div>
+  return (<div 
+    style={{ 
+      color: '#000', 
+      fontFamily: '-apple-system, BlinkMacSystemFont, Roboto, "Segoe UI", "Fira Sans", Avenir, "Helvetica Neue", "Lucida Grande", sans-serif', 
+      height: '100vh', 
+      textAlign: 'center', 
+      display: 'flex', 
+      flexDirection: 'column', 
+      alignItems: 'center', 
+      justifyContent: 'center' }}
+  >
+    <div>
+      <h1 
+        style={{
+          display: 'inline-block', 
+          borderRight: '1px solid rgba(0, 0, 0,.3)', 
+          margin: 0, marginRight: '10px', 
+          padding: '10px 23px 10px 0', 
+          fontSize: '24px', 
+          fontWeight: 500, 
+          verticalAlign: 'top' }}
+      >
+        {code}
+        <span style={{color: '#fff', marginLeft: '20px'}}>|</span>
+      </h1>
+      <div style={{ display: 'inline-block', textAlign: 'left', lineHeight: '49px', height: '49px', verticalAlign: 'middle' }}>
+        <h2 style={{ fontSize: '14px', fontWeight: 'normal', lineHeight: 'inherit', margin: 0, padding: 0 }}>{message}{/* */}
+        </h2>
       </div>
-    </div>);
-  };
+    </div>
+  </div>);
+};
   
 
 export default function FourOhFour() {
   return <>
-     <div>
-            <Head>
-              <style dangerouslySetInnerHTML={{ __html: Styles }} />
-            </Head>
-            <Container className="pt-5 text-center">
-              {errorWrapper(404, 'The page can not be found.')}
-            </Container>
-          </div>
-  </>
+    <div>
+      <Head>
+        <style dangerouslySetInnerHTML={{ __html: Styles }} />
+      </Head>
+      <Container className="pt-5 text-center">
+        {errorWrapper(404, 'The page can not be found.')}
+      </Container>
+    </div>
+  </>;
 }
