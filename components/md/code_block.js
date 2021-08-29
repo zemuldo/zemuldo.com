61625c9f1f8aaa4af80a884667c2d603f8d6cf51
Zemuldo <danstan.otieno@gmail.com>
2021-08-29 12:07:32 +0300

    adds copy code

--- components/md/code_block.js
+++ components/md/code_block.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import darkTheme from 'react-syntax-highlighter/dist/esm/styles/prism/darcula';
 import lightTheme from 'react-syntax-highlighter/dist/esm/styles/prism/ghcolors';
 import CopyCode from './copy_code';
 import { useTheme } from '@material-ui/core';
+import ShouldRender from '../tools/ShoulRender';
 
 const CodeBlock = (props) => {
   const theme = useTheme();
   const { language, value } = props;
+  const [showCopy, setShowCopy] = useState(false);
   return (
-    <div >
+    <div
+      onMouseEnter={() => setShowCopy(true)}
+      onMouseLeave={() => setShowCopy(false)}
+    >
       <div style={{ zIndex: 2, position: 'relative' }}>
-        <CopyCode code={value} />
+        <ShouldRender condition={showCopy}>
+          <CopyCode code={value} />
+        </ShouldRender>
       </div>
       <div  style={{ zIndex: 1, position: 'relative' }}>
         <SyntaxHighlighter
--- components/md/copy_code.js
+++ components/md/copy_code.js
@@ -1,20 +1,31 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import FileCopyIcon from '@material-ui/icons/FileCopy';
-import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import ShouldRender from '../tools/ShoulRender';
 
-const CopyCode = (props) =>{
+const CopyCode = (props) => {
   const [copied, setCopied] = useState(false);
   const handleClick = () => {
     navigator.clipboard.writeText(props.code);
     setCopied(true);
-    setTimeout(()=> setCopied(false), 1500);
+    setTimeout(() => setCopied(false), 1500);
   };
   return (
-    <div style={{ display: 'flex', justifyContent: 'flex-end', width: '100%', padding: '1px', marginBottom: '-40px' }} >
-      <span onClick= {handleClick} style={{cursor: 'pointer',}}>
-        {!copied && <FileCopyIcon fontSize="small" />}
-        {copied && <CheckCircleIcon fontSize="small" />}
+    <div
+      style={{
+        position: 'absolute',
+        display: 'flex',
+        justifyContent: 'flex-end',
+        width: '100%',
+        paddingTop: '8px',
+        paddingRight: '15px',
+        color: '#08a6f3'
+        
+      }}
+    >
+      <span onClick={handleClick} style={{ cursor: 'pointer', fontSize: '18px'}}>
+        <ShouldRender condition={!copied} > Copy </ShouldRender>
+        <ShouldRender condition={copied} > Copied &#10003; </ShouldRender>
+        <span style = {{width: 10, heigh: 10}}></span>
       </span>
     </div>
   );
